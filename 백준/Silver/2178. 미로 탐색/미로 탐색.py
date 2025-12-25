cc1da27e53198c31e13725f9381dfb893c46636c
SEHYUN PARK <79195221+sese2204@users.noreply.github.com>
2025-12-25 16:13:58 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,16 +3,16 @@ dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 n, m = map(int,input().split())
 a = [list(map(int,list(input()))) for _ in range(n)]
+
+
 q = deque()
-dist = [[0]*m for _ in range(n)]
-q.append((0,0))
-dist[0][0]=1
+q.append((0, 0))
 while q:
   x, y = q.popleft()
   for k in range(4):
-    nx, ny = x+dx[k], y+dy[k]
-    if 0<= nx <n and 0<= ny <m:
-      if dist[nx][ny] == 0 and a[nx][ny] == 1:
-        q.append((nx,ny))
-        dist[nx][ny] = dist[x][y]+1
-print(dist[n-1][m-1])
+    nx, ny = x + dx[k], y + dy[k]
+    if 0 <= nx < n and 0 <= ny < m:
+      if a[nx][ny] == 1:
+        q.append((nx, ny))
+        a[nx][ny] = a[x][y] + 1
+print(a[n-1][m-1])
